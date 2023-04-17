6b25a27f68317f9eb23869c7f6cb7c869e25b4cc
Kuanysh K <kaimoldin.k@qazdevelop.com>
2023-04-17 17:30:02 +0000

    GL-2: add authorization server configuration

--- src/main/java/com/example/app/configs/SecurityConfig.java
+++ src/main/java/com/example/app/configs/SecurityConfig.java
@@ -2,39 +2,44 @@ package com.example.app.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
-				.cors().disable()
-				.csrf()
-				.and()
-				.authorizeHttpRequests()
-				.requestMatchers("/", "/register", "/css/**").permitAll()
-				.anyRequest().authenticated()
-				.and()
-				.formLogin(formLogin -> formLogin
-						.loginPage("/login")
-						.failureUrl("/login-error")
-						.defaultSuccessUrl("/")
-						.permitAll())
-				.logout()
-				.logoutSuccessUrl("/login");
-		return http.build();
-	}
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    @Order(2)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors().disable()
+                .csrf()
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/", "/register", "/css/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .failureUrl("/login-error")
+                        .defaultSuccessUrl("/")
+                        .permitAll())
+                .logout()
+                .logoutSuccessUrl("/login");
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
